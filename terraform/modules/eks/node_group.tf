e8912917a169344a1376d470e63805f67594d5d4
Hieuej147 <hihigani@gmail.com>
2026-09-08 10:27:24 +0700

    fix(eks): use ON_DEMAND t3.medium for node group to avoid new-account Spot restrictions

--- terraform/modules/eks/node_group.tf
+++ terraform/modules/eks/node_group.tf
@@ -13,8 +13,8 @@ resource "aws_eks_node_group" "node_group" {
   subnet_ids      = var.eks_subnet_ids
   ami_type        = "AL2023_x86_64_STANDARD"
   release_version = data.aws_ssm_parameter.eks_ami_release_version.value
-  capacity_type   = "SPOT"
-  instance_types  = ["t3.medium", "t3a.medium"]
+  capacity_type   = "ON_DEMAND"
+  instance_types  = ["t3.medium"]
 
   scaling_config {
     min_size     = 1
